74c3f6b6917075af0ed5b93d93b8e9163eabe7bb
kyASse <akyasmuhamadzaidan59@gmail.com>
2026-05-19 14:19:46 +0700

    chore: remove standalone output configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,6 @@ const supabaseHostname = (() => {
 })();
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   // Properti lain yang mungkin sudah Anda miliki bisa tetap ada di sini
   // Enforce ESLint during builds
   eslint: {},
