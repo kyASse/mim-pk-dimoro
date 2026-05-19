22858c92fc8cb0f57b0d361a7f3d7867222f21e9
kyASse <akyasmuhamadzaidan59@gmail.com>
2026-05-19 13:50:52 +0700

    feat: add default admin account creation to seed file

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -14,3 +14,44 @@ VALUES
   ('Dana Pendidikan', 1500000, 1500000),
   ('Seragam', 600000, 650000)
 ON CONFLICT DO NOTHING;
+
+-- Buat Akun Admin Default (admin@mimpkdimoro.sch.id / admin123)
+INSERT INTO
+    auth.users (
+        instance_id,
+        id,
+        aud,
+        role,
+        email,
+        encrypted_password,
+        email_confirmed_at,
+        recovery_sent_at,
+        last_sign_in_at,
+        raw_app_meta_data,
+        raw_user_meta_data,
+        created_at,
+        updated_at,
+        confirmation_token,
+        email_change,
+        email_change_token_new,
+        recovery_token
+    ) (
+        select
+            '00000000-0000-0000-0000-000000000000',
+            uuid_generate_v4 (),
+            'authenticated',
+            'authenticated',
+            'admin@mimpkdimoro.sch.id',
+            crypt ('admin123', gen_salt ('bf')),
+            current_timestamp,
+            current_timestamp,
+            current_timestamp,
+            '{"provider":"email","providers":["email"]}',
+            '{}',
+            current_timestamp,
+            current_timestamp,
+            '',
+            '',
+            '',
+            ''
+    );
