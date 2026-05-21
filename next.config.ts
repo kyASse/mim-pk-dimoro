21e7834c69e314568d4a73d387187fa61e58d0b0
kyASse <akyasmuhamadzaidan59@gmail.com>
2026-05-21 16:49:26 +0700

    fix: add image configuration options for SVG support and security policies

--- next.config.ts
+++ next.config.ts
@@ -43,6 +43,9 @@ const nextConfig: NextConfig = {
   },
 
   images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     domains: ["tk-aba-mertosanan.sch.id", "images.pexels.com", "placehold.co"],
     remotePatterns: [
       ...(supabaseHostname
